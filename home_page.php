134b22999a8d7f2c3d9c8edc48ed2681f8a7efe9
projectbrady <brady.wemette@gmail.com>
2026-08-06 11:20:50 -0500

    fixed upper case of user names

--- home_page.php
+++ home_page.php
@@ -10,19 +10,24 @@ if (!isset($_SESSION['email'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="home_container"> 
+    <div class="home_container">
         <h1>
-        Welcome back,
-        <span><?= $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?></span>
+            Welcome back,
+            <span>
+                <?= ucwords(strtolower($_SESSION['first_name'] . ' ' . $_SESSION['last_name'])); ?>
+            </span>
         </h1>
         <button onclick="window.location.href='logout.php'">Logout</button>
     </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -23,90 +23,94 @@ function isActiveForm($formName, $activeForm) {
 
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
-  <title>Login</title>
+    <title>Login</title>
 </head>
 
 <body>
 
-<div class="main-container">
+    <div class="main-container">
 
-  <div class="form-wrapper">
+        <div class="form-wrapper">
 
-    <form action="login_register.php" class="form <?= isActiveForm('login', $activeForm) ?>" id="login" method="post">
+            <form action="login_register.php" class="form <?= isActiveForm('login', $activeForm) ?>" id="login"
+                method="post">
 
-      <h1>Log In</h1>
+                <h1>Log In</h1>
 
-      <?= showError($errors['login']); ?>
+                <?= showError($errors['login']); ?>
 
-      <label>Email</label>
-      <input type="email" name="email" placeholder="John.Doe@email.com" required>
+                <label>Email</label>
+                <input type="email" name="email" placeholder="John.Doe@email.com" required>
 
-      <label>Password</label>
+                <label>Password</label>
 
-      <div class="password-container">
-        <input class="password-input" type="password" name="password" placeholder="********" required>
-        <i class="fa-solid fa-eye toggle-password"></i>
-      </div>
+                <div class="password-container">
+                    <input class="password-input" type="password" name="password" placeholder="********" required>
+                    <i class="fa-solid fa-eye toggle-password"></i>
+                </div>
 
-      <input type="submit" name="login" value="Log In">
+                <input type="submit" name="login" value="Log In">
 
-      <span class="switch-text">
-        Don't have an account?
-        <a href="#" class="form-toggle">Create One!</a>
-      </span>
+                <span class="switch-text">
+                    Don't have an account?
+                    <a href="#" class="form-toggle">Create One!</a>
+                </span>
 
-    </form>
+            </form>
 
 
-    <form action="login_register.php" class="form <?= isActiveForm('register', $activeForm) ?>" id="signup" method="post">
+            <form action="login_register.php" class="form <?= isActiveForm('register', $activeForm) ?>" id="signup"
+                method="post">
 
-      <h1>Create Account</h1>
+                <h1>Create Account</h1>
 
-      <?= showError($errors['register']); ?>
+                <?= showError($errors['register']); ?>
 
-      <label>First Name</label>
-      <input type="text" name="fname" placeholder="First Name..." required>
+                <label>First Name</label>
+                <input type="text" name="fname" placeholder="First Name..." required>
 
-      <label>Last Name</label>
-      <input type="text" name="lname" placeholder="Last Name..." required>
+                <label>Last Name</label>
+                <input type="text" name="lname" placeholder="Last Name..." required>
 
-      <label>Email</label>
-      <input type="email" name="email" placeholder="John.Doe@email.com" required>
+                <label>Email</label>
+                <input type="email" name="email" placeholder="John.Doe@email.com" required>
 
-      <label>Password</label>
+                <label>Password</label>
 
-      <div class="password-container">
-        <input class="password-input" type="password" name="password" placeholder="********" required>
-        <i class="fa-solid fa-eye toggle-password"></i>
-      </div>
+                <div class="password-container">
+                    <input class="password-input" type="password" name="password" placeholder="********" required>
+                    <i class="fa-solid fa-eye toggle-password"></i>
+                </div>
 
-      <input type="submit" name="register" value="Sign Up">
+                <input type="submit" name="register" value="Sign Up">
 
-      <span class="switch-text">
-        Already have an account?
-        <a href="#" class="form-toggle">Log In!</a>
-      </span>
+                <span class="switch-text">
+                    Already have an account?
+                    <a href="#" class="form-toggle">Log In!</a>
+                </span>
 
-    </form>
+            </form>
 
-  </div>
+        </div>
 
-</div>
+    </div>
 
-<script src="script.js"></script>
+    <script src="script.js"></script>
 
 </body>
+
 </html>
